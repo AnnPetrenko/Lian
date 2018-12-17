18f2ae430b79e4ed8b9c733b5d2fe819e224fa7f
AnnPetrenko <petrenkoann90@gmail.com>
2018-12-17 21:40:34 +0200

    07_contact.map

--- js/script.js
+++ js/script.js
@@ -17,10 +17,10 @@ $(document).ready(function(){
                 items:1
             },
             600:{
-                items:1
+                items:3
             },
             1000:{
-                items:1
+                items:3
             }
         }
     });
